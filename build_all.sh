1ef14271c18b7ce559e4e90c21862ccba492d905
Tom Davidson <tjd@alum.mit.edu>
2018-07-13 13:53:28 -0700

    Fix Mac-specific shell script error

--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,3 @@
-#!/bin/bash -euxo pipefail
+#!/bin/bash
+set -u -x -o pipefail
 conda build --skip-existing -c tjd2002 recipe.mountainlab/ recipe.isosplit5/ recipe.mountainlab_pytools/ recipe.ml_ephys/ recipe.ml_ms4alg/ recipe.ml_ms3/ recipe.qt-mountainview/
