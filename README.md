b1f9729067a3edb399fedfb0dd54ce1021990436
Tom Davidson <tjd@alum.mit.edu>
2018-07-19 15:53:58 -0700

    Big refactor: top-level conda_build_config, use conda compilers,...

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # mountainlab-conda
 
 A collection of recipes for building conda packages for [Mountainlab](https://github.com/flatironinstitute/mountainlab-js) and Mountainsort
+
+Important config information is provided in the top-level `conda_build_config.yaml` and `recipe_append.yaml files`, so you must build the recipes from this directory
+
+To build (in a conda env with `conda-build` installed):
+```
+cd /path/to/mountainlab-conda/
+./build_all.sh
+```
+
+After all packages are built, this will create a files list (linked from pkglist_latest) that can be used to upload packages to anaconda.org:
+
+In a conda env with `anaconda-client` installed, and with login credentials stored:
+```
+anaconda upload `cat pkglist_latest`
+```
+
+(This file list is also helpful for upating version #s in the 'mountainsort' metapackage)
--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-set -u -x -o pipefail
-conda build --skip-existing -c tjd2002 recipe.mountainlab/ recipe.isosplit5/ recipe.mountainlab_pytools/ recipe.ml_ephys/ recipe.ml_ms4alg/ recipe.ml_ms3/ recipe.qt-mountainview/
+set -e -u -x -o pipefail
+RECIPE_DIRS_ORDERED="\
+recipe.isosplit5 \
+recipe.mountainlab \
+recipe.mountainlab_pytools \
+recipe.ml_ephys \
+recipe.ml_ms4alg \
+recipe.ml_ms3 \
+recipe.qt-mountainview \
+recipe.ephys-viz \
+recipe.mountainsort \
+"
+
+# avoid bootstrapping with existing published packages (i.e. don't include tjd2002/flatiron)
+# (but local always included--may need to blow away conda-bld if needed)
+channel_args="-c defaults -c conda-forge --override-channels"
+append_args="--append-file=recipe_append.yaml"
+conda build --check $channel_args $append_args $RECIPE_DIRS_ORDERED || exit 2
+conda build --skip-existing $channel_args $append_args $RECIPE_DIRS_ORDERED || exit 3
+
+# Get filenames of all packages (even if they were skipped by '--skip-existing')
+tmpfile=`mktemp ./pkglist.XXXXXX`
+conda build --output $channel_args $append_args $RECIPE_DIRS_ORDERED>$tmpfile
+ln -sf $tmpfile pkglist_latest
+
+echo 'Run "anaconda upload `cat pkglist_latest`" to upload all packages'
