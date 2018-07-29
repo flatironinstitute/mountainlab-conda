a36f14c3800dc3ce8db4809e7101659244bca003
Tom Davidson <tjd@alum.mit.edu>
2018-07-29 11:29:09 -0700

    Bump: Mountainsort 4.2.0, mountainlab 0.13.0. Simplify de/activation as workaround for #1

--- build_all.sh
+++ build_all.sh
@@ -7,6 +7,7 @@ recipe.mountainlab_pytools \
 recipe.ml_ephys \
 recipe.ml_ms4alg \
 recipe.ml_ms3 \
+recipe.ml_pyms \
 recipe.qt-mountainview \
 recipe.ephys-viz \
 recipe.mountainsort \
@@ -14,7 +15,7 @@ recipe.mountainsort \
 
 # avoid bootstrapping with existing published packages (i.e. don't include tjd2002/flatiron)
 # (but local always included--may need to blow away conda-bld if needed)
-channel_args="-c defaults -c conda-forge --override-channels"
+channel_args="-c defaults -c conda-forge -c local -c flatiron --override-channels"
 append_args="--append-file=recipe_append.yaml"
 conda build --check $channel_args $append_args $RECIPE_DIRS_ORDERED || exit 2
 conda build --skip-existing $channel_args $append_args $RECIPE_DIRS_ORDERED || exit 3
