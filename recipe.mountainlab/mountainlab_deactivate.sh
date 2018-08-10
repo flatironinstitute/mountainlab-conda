19875a1f64c79a3c0ad1daab51ffc986e692d415
Jeremy Magland <jeremy.magland@gmail.com>
2018-08-09 23:15:46 -0400

    change system for installing mountainlab packages

--- recipe.mountainlab/mountainlab_deactivate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#echo "=== [ mountainlab deactivation ] ==========================================="
-#echo ":"
-
-unset ML_CONFIG_DIRECTORY 
-
-unset ML_CONDA_DIR
-unset ML_CONDA_PACKAGES_DIR
-unset ML_CONDA_DATABASE_DIR
